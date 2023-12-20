86084e14184ceb1ec7bbfed6846535866bc51c30
jackomonkey <bwallsisaboss@gmail.com>
2023-12-20 14:37:46 -0600

    Fixed input and currently adding forces. Still BUGGGYgit statusgit status!

--- Air.gd
+++ Air.gd
@@ -7,10 +7,6 @@ func _process(delta):
 	#get_node("RigidBody2D").gravity_scale=0
 	Mouse_Position=get_global_mouse_position()
 	global_position=Mouse_Position
-	Bubble_Position=get_parent().get_node("Bubble").global_position
+	Bubble_Position=get_parent().get_node("Bubble/BubbleRigid").global_position
 	look_at(Bubble_Position)
-	if (Input.is_action_pressed("Blow_air")):
-		#print("SUCCESS")
-		Bubble_Position.x+=100
-		pass
 	pass
--- PushBubble.gd
+++ PushBubble.gd
@@ -2,15 +2,22 @@ extends RayCast2D
 
 var Mouse_Position
 var Bubble_Position
+var force=1
+var velocity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	Mouse_Position=get_global_mouse_position()
-	Bubble_Position=get_parent().get_parent().get_node("Bubble")
-	if is_colliding():
-		#if get_collider("")
-		#print("YUES")
-		#Bubble_Position.global_translate(Vector2(1,1))
-		pass
-	
-	pass
+	Mouse_Position=get_parent().global_position - get_parent().get_parent().get_node("Bubble").global_position
+	#Bubble_Position=get_parent().get_parent().get_node("Bubble")
+	velocity=get_parent().get_parent().get_node("Bubble/BubbleRigid").get_linear_velocity()
+	print("velocity: ", velocity)
+	print("Mouse: ", Mouse_Position)
+	#print("Mouse Normalized: ", Mouse_Position.normalized())
+	# Adding force when raycast detects bubble and mouse button pressed
+	if get_collider()==get_parent().get_parent().get_node("Bubble/BubbleRigid") and Input.is_action_pressed("Blow_air"):
+		get_parent().get_parent().get_node("Bubble/BubbleRigid").add_constant_force(-Mouse_Position)
+	elif(velocity!=Vector2(0,0)): # This is for stopping the bubble to a halt when not pushing air.
+		get_parent().get_parent().get_node("Bubble/BubbleRigid").set_linear_velocity(lerp(velocity,Vector2(0,0),0.01))
+
+func _integrate_forces(state):
+	get_parent().get_parent().get_node("Bubble/BubbleRigid").rotation_degree = 0
