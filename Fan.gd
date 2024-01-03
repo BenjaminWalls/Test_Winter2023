3b66996fba10c52938632bd3d9ac4d4d9aba1204
jackomonkey <bwallsisaboss@gmail.com>
2024-01-02 19:18:55 -0600

    Added fans

---
+++ Fan.gd
@@ -0,0 +1,14 @@
+extends ShapeCast2D
+
+
+var Fan_Position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	Fan_Position=get_parent().global_position - get_parent().get_parent().get_parent().get_parent().get_node("Bubble/BubbleRigid").global_position
+	#print(get_collider(0))
+	if get_collider(0)==get_parent().get_parent().get_parent().get_parent().get_node("Bubble/BubbleRigid"):
+		#print(Fan_Position)
+		get_parent().get_parent().get_parent().get_parent().get_node("Bubble/BubbleRigid").apply_central_force(-Fan_Position*1.4)
+	
+	pass
