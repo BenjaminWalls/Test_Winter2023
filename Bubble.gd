847bb3c4ecec41d7e6bdbed9c0959482350dd91b
jackomonkey <bwallsisaboss@gmail.com>
2024-01-03 15:12:17 -0600

    Background added (poorly)

--- Bubble.gd
+++ Bubble.gd
@@ -19,4 +19,5 @@ func _on_bubble_area_area_entered(area):
 		get_tree().reload_current_scene()
 	elif area.name=="Sphere_Area":
 		get_tree().change_scene_to_file("res://menu.tscn")
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
 		pass
